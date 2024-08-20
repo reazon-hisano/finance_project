081401a000ebdaf7355e1ade1a33a25cf2bcd2ac
reazon-hisano <yasutaka_hisano@reazon.jp>
2024-08-20 11:35:20 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ def run_selenium():
     
     # Extract parameters from the request body
     report_type = data.get('report_type')
+    email = data.get('email')
     release_date = data.get('release_date')
     country = data.get('country')
     app_type = data.get('app_type')
@@ -20,7 +21,7 @@ def run_selenium():
     
     # Run the Selenium script with the provided data
     try:
-        run_selenium_script(report_type, release_date, country, app_type, number_of_apps, rank_lower, rank_upper)
+        run_selenium_script(report_type, email, release_date, country, app_type, number_of_apps, rank_lower, rank_upper)
         return jsonify({"status": "success", "message": "Selenium script executed successfully."}), 200
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
