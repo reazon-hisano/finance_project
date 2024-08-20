2e373a31da0160d3247d5e3a17bb3615e2ebef33
reazon-hisano <yasutaka_hisano@reazon.jp>
2024-08-20 11:30:03 +0900

    Add files via upload

--- selenium_script.py
+++ selenium_script.py
@@ -7,9 +7,14 @@ from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 import time
 
-def run_selenium_script(report_type, release_date, country, app_type, number_of_apps, rank_lower, rank_upper):
+def run_selenium_script(report_type, email, release_date, country, app_type, number_of_apps, rank_lower, rank_upper):
     # Path to your ChromeDriver
     # options = Options()
     # options.add_argument('--headless')  # Enable headless mode
@@ -23,44 +28,84 @@ def run_selenium_script(report_type, release_date, country, app_type, number_of_
 
     driver.get("https://investment-ten-self.vercel.app/")
 
-    # Wait for the page to load
     time.sleep(2)
 
-    # Fill out the form fields
-    # Select Report Type (Example: Top Chart or Keyword Top Chart)
-    report_type_select = Select(driver.find_element(By.XPATH, "//select[@aria-label='Report Type']"))
-    report_type_select.select_by_value(report_type)  # "1" for Top Chart, "2" for Keyword Top Chart
-
+    # Select Report Type
+    report_type_element = driver.find_element(By.XPATH, "//*[@id='root']/div/div/div")
+    report_type_element.click()
+    # time.sleep(2)
+    report_type_option = driver.find_element(By.XPATH, "//*[@id=':r0:']/li[2]")
+    report_type_option.click()
+    # time.sleep(2)
+    email_field = driver.find_element(By.XPATH, "//*[@id='root']/div/div[2]/div")
+    email_field.click()
+    driver.find_element(By.XPATH, "//*[@id=':r1:']").send_keys(email)
+    # time.sleep(2)
     # Fill Release Date
-    release_date_field = driver.find_element(By.XPATH, "//input[@label='Release Date']")
-    release_date_field.send_keys(release_date)
+    release_date_field = driver.find_element(By.XPATH, "//*[@id='root']/div/div[3]/div")
+    release_date_field.click()
+    driver.find_element(By.XPATH, "//*[@id=':r2:']").send_keys(release_date)
+    # time.sleep(2)
 
     # Select Country
-    country_select = Select(driver.find_element(By.XPATH, "//select[@aria-label='Country']"))
-    country_select.select_by_value(country)
+    country_element = driver.find_element(By.XPATH, "//*[@id='root']/div/div[4]/div")
+    country_element.click()
+    country_option = driver.find_element(By.XPATH, "//*[@id=':r3:']/li[2]")
+    country_option.click()
+    # time.sleep(2)
 
-    # Select App Type (free or paid)
-    app_type_select = Select(driver.find_element(By.XPATH, "//select[@aria-label='App Type']"))
-    app_type_select.select_by_value(app_type)
+    # Select App Type
+    app_type_element = driver.find_element(By.XPATH, "//*[@id='root']/div/div[5]/div")
+    app_type_element.click()
+    app_type_option = driver.find_element(By.XPATH, "//*[@id=':r4:']/li[2]")
+    app_type_option.click()
+    # time.sleep(2)
 
     # Number of Apps
-    num_apps_select = Select(driver.find_element(By.XPATH, "//select[@aria-label='Number of Apps']"))
-    num_apps_select.select_by_value(str(number_of_apps))
+    # num_apps_element = driver.find_element(By.XPATH, "//*[@id='root']/div/div[6]/div")
+    # num_apps_element.click()
+    # num_apps_option = driver.find_element(By.XPATH, "//*[@id=':r5:']/li[4]")
+    # ActionChains(driver)\
+    #     .scroll_to_element(num_apps_option)\
+    #     .perform()
+    # num_apps_option.click()
+    # time.sleep(2)
 
     # Rank Lower Limit
-    rank_lower_select = Select(driver.find_element(By.XPATH, "//select[@aria-label='Rank Lower Limit']"))
-    rank_lower_select.select_by_value(str(rank_lower))
+    rank_lower_element = driver.find_element(By.XPATH, "//*[@id='root']/div/div[7]/div[1]")
+    rank_lower_element.click()
+    rank_lower_option = driver.find_element(By.XPATH, "//*[@id=':r6:']/li[5]")
+    ActionChains(driver)\
+        .scroll_to_element(rank_lower_option)\
+        .perform()
+    rank_lower_option.click()
 
     # Rank Upper Limit
-    rank_upper_select = Select(driver.find_element(By.XPATH, "//select[@aria-label='Rank Upper Limit']"))
-    rank_upper_select.select_by_value(str(rank_upper))
+    rank_upper_element = driver.find_element(By.XPATH, "//*[@id='root']/div/div[7]/div[2]")
+    rank_upper_element.click()
 
-    # Submit the form by clicking the 'Create Report' button
-    create_report_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Create Report')]")
+    rank_upper_option = driver.find_element(By.XPATH, "//*[@id=':r7:']/li[150]")
+    ActionChains(driver)\
+        .scroll_to_element(rank_upper_option)\
+        .perform()
+    rank_upper_option.click()
+
+    num_apps_element = driver.find_element(By.XPATH, "//*[@id='root']/div/div[6]")
+    num_apps_element.click()
+    num_apps_option = driver.find_element(By.XPATH, "//*[@id=':r5:']/li[20]")
+    ActionChains(driver)\
+        .scroll_to_element(num_apps_option)\
+        .perform()
+    num_apps_option.click()
+
+    # Submit the form
+    create_report_button = driver.find_element(By.XPATH, "//*[@id='root']/div/button")
     create_report_button.click()
 
-    # Wait for the response (this could vary depending on the site)
-    time.sleep(100)
-
+    # Wait for the response
+    time.sleep(10)
+    delay = 3 # seconds
+    WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, "//*[@id='root']/div/div/div[1]")))
+    time.sleep(3)
     # Close the browser
     driver.quit()
